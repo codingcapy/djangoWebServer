e2e06ce0bb1195d4373191aae1b51eabd67f8a52
Paul K <spkim0921@gmail.com>
2024-04-30 19:35:16 -0700

    2nd commit

--- djangoApp/views.py
+++ djangoApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.core.serializers import serialize
+from .models import User
 
 # Create your views here.
 def home(request):
     return HttpResponse("welcome")
+
+def users(request):
+    users = User.objects.all()
+    users_json = serialize('json', users)
+    return JsonResponse(users_json, safe=False)
